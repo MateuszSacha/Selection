041258aaaa4340f58cd21b3b145e3b2fa1fa3af7
Mateusz Sacha <45197@longroad.ac.uk>
2014-09-30 16:00:57 +0100

    taks one done

---
+++ task 1.py
@@ -0,0 +1,33 @@
+#mateusz sacha
+#30/09/2014
+#selection class exercises. task 1
+month = int(input("Please enter a month as a number between 1-12: "))
+if month == 1:
+    print("The month you entered is January")
+elif month == 2:
+    print("The month you entered is February")
+elif month == 3:
+    print("The month you entered is March")
+elif month == 4:
+    print("The month you entered is April")
+elif month == 5:
+    print("The month you entered is May")
+elif month == 6:
+    print("The month you entered is June")
+elif month == 7:
+    print("The month you entered is July")
+elif month == 8:
+    print("The month you entered is August")
+elif month == 9:
+    print("The month you entered is September")
+elif month == 10:
+    print("The month you entered is October")
+elif month == 11:
+    print("The month you entered is November")
+elif month == 12:
+    print("The month you entered is December")
+elif month < 1:
+    print("You have entered a number which is less than 1. Please enter a number between 1 ans 12")
+elif month > 12:
+    print("You have entered anumber which is greater than 12. Please enter a number between 1 and 12")
+    
