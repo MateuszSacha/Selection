24218e357291eda6a9ded6a241e9bf3fd70f2a0e
Mateusz Sacha <45197@longroad.ac.uk>
2014-10-01 15:15:50 +0100

    on revision task 2

---
+++ revision task 1.py
@@ -0,0 +1,8 @@
+# Mateusz Sacha
+# 01/10/2014
+# selection revision exercise 1
+number = int(input("Enter a number"))
+number2 = int(input("Enter a number"))
+if number == number2:
+    print("Congratulations you have entered two number which are the same!")
+    
