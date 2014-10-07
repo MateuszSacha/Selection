e0739a021c78cba000d93bb2a80fbf253fca0afb
Laptop <Laptop@Laptop-PC>
2014-10-07 21:36:58 +0100

    finished development exercise 3.

---
+++ development exercise 3.py
@@ -0,0 +1,9 @@
+# Mateusz Sacha
+# 07-10-2014
+# Development Exercise 3
+hours = int(input("Enter the number of hours you worked this week:"))
+rate_of_pay = int(input("Enter your hourly rate of pay:"))
+answer = hours * rate_of_pay
+print("{0}: This is how much you have earned this week.".format(answer))
+
+
